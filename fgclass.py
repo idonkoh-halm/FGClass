daafb13791604cb3197f64c205284e371e9b8d99
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2016-01-04 13:15:33 -0500

    Listing the population

--- fgclass.py
+++ fgclass.py
@@ -6,13 +6,14 @@ class Game:
     graveyard = []
 
     def __init__(self):
-        self.played = False
+        self.played = True
         self.population.append(self)
         self.reccomend = False
     def local(self):
         if self.played==True:
             self.population.remove(self)
             self.fgc_players.append(self)
+            
     def sax(self):
         self.population.remove(self)
         self.fgc_players.append(self)
@@ -30,20 +31,22 @@ class Game:
         if self.played:
             self.reccomend = True
         if self.played:
-           toss=random.randint(1,2)
-           if toss==1:
-               self.sax()
-           if toss==2:
-                self.played=False
-                self.reccomend=False
+            self.sax()
+            #toss=random.randint(1,2)
+            #if toss==1:
+            #   self.sax()
+            #if toss==2:
+            #    self.played=False
+            #    self.reccomend=False
 
         friends=self.population[:]
         for friend in friends:
             if friend.reccomend:
                 self.sax=True
-def create_FGC_Players (self):
-    GIMR = Game()
-    GIMR.played=True
+        if self.played:
+            self.local()
+    def create_FGC_Players (self):        
+        self.played=True
     
 def run_simulation (population,ticks):
     create_FGC_Players(population)
@@ -51,7 +54,14 @@ def run_simulation (population,ticks):
         print len (Game.population)
         for p in Game.population:
             p.tick()
+            
+
 
     
 
-run_simulation(500,3)
+#run_simulation(500,10)
+
+for i in range (10):
+    Game()
+print len (Game.fgc_players)
+print len (Game.population)
