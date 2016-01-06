b93f34b78b908b6bf846a779b83cdf0569933bd4
idonkoh-halm <isaac.donkoh-halm@innovationcharter.org>
2016-01-06 10:40:35 -0500

    work

--- fgclass.py
+++ fgclass.py
@@ -1,37 +1,25 @@
 import random
-class Game:
+class GamePlayer:
 
     population = []
     fgc_players = []
     graveyard = []
 
     def __init__(self):
-        self.played = True
+        self.played = False
         self.population.append(self)
         self.reccomend = False
-    def local(self):
-        if self.played==True:
-            self.population.remove(self)
-            self.fgc_players.append(self)
-            
-    def sax(self):
+    def local_tourney(self):
         self.population.remove(self)
         self.fgc_players.append(self)
-            
-    def injustice (self):
-        self.population.remove(self)
-        self.graveyard.append(self)
-    def death_tick(self):
-        warner=0
-        if warner==0:
-            for i in range (300):
-                self.injustice()
-                print graveyard
+#    def injustice (self):
+#        self.population.remove(self)
+#        self.graveyard.append(self)
     def tick(self):
         if self.played:
             self.reccomend = True
         if self.played:
-            self.sax()
+            self.local()
             #toss=random.randint(1,2)
             #if toss==1:
             #   self.sax()
@@ -39,29 +27,35 @@ class Game:
             #    self.played=False
             #    self.reccomend=False
 
-        friends=self.population[:]
-        for friend in friends:
-            if friend.reccomend:
-                self.sax=True
-        if self.played:
-            self.local()
+       # friends=self.population[:]
+        #for friend in friends:
+            #if friend.reccomend:
+                #self.sax=True
+        #if self.played:
+        #    self.local()
     def create_FGC_Players (self):        
         self.played=True
+
+def make_players (n):
+    fgc=GamePlayer()
+    fgc.played=True
+    for q in range (n-1):
+        GamePlayer()
     
 def run_simulation (population,ticks):
-    create_FGC_Players(population)
+    make_players(population)
     for t in range(ticks):
-        print len (Game.population)
-        for p in Game.population:
+        print len (GamePlayer.population)
+        for p in GamePlayer.population:
             p.tick()
             
 
 
     
 
-#run_simulation(500,10)
+run_simulation(50,10)
 
 for i in range (10):
-    Game()
-print len (Game.fgc_players)
-print len (Game.population)
+    GamePlayer()
+print len (GamePlayer.fgc_players)
+print len (GamePlayer.population)
